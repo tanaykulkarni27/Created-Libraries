d7d6e640fdcf1d1065a7ee9a4c105b193cbe3558
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-10-29 22:09:34 +0530

    Create GraphAPI.js

---
+++ GraphAPI.js
@@ -0,0 +1,17 @@
+class Graph{
+    constructor(data,total){
+        this.data = data;
+        this.total = total;
+    }
+    getgraph() {
+    var j =0;
+    var table = '<table> <tr>';
+       for (j=0;j<this.data.length;j++){
+        var percent = (this.data[j]/this.total)*100;
+        var table_cols = '<td style="vertical-align: bottom;"><div style="background-color:red;width:14px;height:'+percent+'px;"></div></td>'; 
+         table= table+table_cols;
+   }
+       table+='</tr></table>';
+       return table; 
+    }
+    }
