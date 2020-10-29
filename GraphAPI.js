2793f471b37815fa966f55dfd7e8359a828016c4
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-10-29 22:11:25 +0530

    Update GraphAPI.js

--- GraphAPI.js
+++ GraphAPI.js
@@ -8,8 +8,8 @@ class Graph{
     var table = '<table> <tr>';
        for (j=0;j<this.data.length;j++){
         var percent = (this.data[j]/this.total)*100;
-        var table_cols = '<td style="vertical-align: bottom;"><div style="background-color:red;width:14px;height:'+percent+'px;"></div></td>'; 
-         table= table+table_cols;
+        var table_cols = '<td style="vertical-align: bottom;column-gap: 20px;">'+this.data[j]+'<div style="background-color:red;width:14px;height:'+percent+'px;"></div></td>'; 
+        table= table+table_cols;
    }
        table+='</tr></table>';
        return table; 
